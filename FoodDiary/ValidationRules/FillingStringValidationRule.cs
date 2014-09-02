69a187a9fbd0ad109c27bad9b7e114288324bf79
pimenov-eg <pimenov.eg@gmail.com>
2014-09-02 15:27:16 +0400

    Правил валидации и шаблон ошибок валидации

---
+++ FoodDiary/ValidationRules/FillingStringValidationRule.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace FoodDiary
+{
+  /// <summary>
+  /// Правило валидации на проверку заполненности строки.
+  /// </summary>
+  public class FillingStringValidationRule : ValidationRule
+  {
+    #region Методы
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+      string stringValue = string.Empty;
+
+      try
+      {
+        stringValue = (string)value;
+      }
+      catch
+      {
+        return new ValidationResult(false, "Incorrect format.");
+      }
+
+      if (string.IsNullOrWhiteSpace(stringValue))
+        return new ValidationResult(false, "Value mast be a string.");
+      else
+        return new ValidationResult(true, null);
+    }
+
+    #endregion
+
+    #region Конструкторы
+
+    public FillingStringValidationRule()
+    {
+    }
+
+    #endregion
+  }
+}
---
+++ FoodDiary/ValidationRules/IntRangeValidationRule.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace FoodDiary
+{
+  /// <summary>
+  /// Правило валидации на проверку вхождения в заданный интервал целых чисел.
+  /// </summary>
+  public class IntRangeValidationRule : ValidationRule
+  {
+    #region Константы
+
+    /// <summary>
+    /// Значение по умолчанию для минимального значения.
+    /// </summary>
+    public const int DefaultMinValue = 0;
+
+    /// <summary>
+    /// Значение по умолчанию для максимального значения.
+    /// </summary>
+    public const int DefaultMaxValue = 1000;
+
+    #endregion
+
+    #region Поля и свойства
+
+    /// <summary>
+    /// Минимальное значение.
+    /// </summary>
+    public int MinValue { get; set; }
+
+    /// <summary>
+    /// Максимальное значение.
+    /// </summary>
+    public int MaxValue { get; set; }
+
+    #endregion
+
+    #region Методы
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+      int convertedValue = 0;
+
+      try
+      {
+        if (((string)value).Length > 0)
+          convertedValue = Int32.Parse((String)value);
+      }
+      catch
+      {
+        return new ValidationResult(false, "Incorrect format.");
+      }
+
+      if (convertedValue < this.MinValue || convertedValue > this.MaxValue)
+        return new ValidationResult(false, "Value mast be in the range: " + MinValue + " - " + MaxValue + ".");
+      else
+        return new ValidationResult(true, null);
+    }
+
+    #endregion
+
+    #region Конструкторы
+
+    public IntRangeValidationRule()
+    {
+      this.MinValue = DefaultMinValue;
+      this.MaxValue = DefaultMaxValue;
+    }
+
+    #endregion
+  }
+}
