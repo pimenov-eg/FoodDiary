743cd873d0e5ad651a9374f9f32d1d57d6ff8cda
pimenov-eg <pimenov.eg@gmail.com>
2014-09-04 17:49:15 +0400

    Фильтрация в списке продуктов

--- FoodDiary/View/FoodDiaryView.xaml.cs
+++ FoodDiary/View/FoodDiaryView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 using Google;
 
 namespace FoodDiary.View
@@ -29,6 +18,35 @@ namespace FoodDiary.View
     private void HyperlinkClickHandler(object sender, RoutedEventArgs e)
     {
       new AuthorizationWindow().ShowDialog();
+    }
+
+    private void ComboBoxLoadedHandler(object sender, RoutedEventArgs e)
+    {
+      // http://www.codeproject.com/Questions/410084/Autocomplete-Combobox-in-WPF
+      var comboBox = (ComboBox)sender;
+      var textBox = comboBox.Template.FindName("PART_EditableTextBox", comboBox) as TextBox;
+      var popup = comboBox.Template.FindName("PART_Popup", comboBox) as Popup;
+      if (textBox != null)
+      {
+        textBox.TextChanged += delegate
+        {
+          comboBox.Items.Filter += (item) =>
+          {
+            if (item.ToString().StartsWith(textBox.Text))
+            {
+              popup.IsOpen = true;
+              return true;
+
+            }
+            else
+            {
+              // popup.IsOpen = false;
+              return false;
+            }
+          };
+
+        };
+      }
     }
   }
 }
